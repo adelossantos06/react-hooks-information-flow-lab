42d5c19abb31251fb91f36cd96105264d572ee50
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-10-28 18:54:06 -0500

    complete information flow lab

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import App from "./App"
+
+
+function Header({ onDarkModeClick, isDarkMode }) {
+
+
+    return (
+        <header >
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick} >
+                {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    );
+}
+
+
+
+
+export default Header;
